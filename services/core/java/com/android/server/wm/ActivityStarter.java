7e48f13f0cc4e42719ee028979c4c8e0ab92d53f
DennySPB <dennyspb@gmail.com>
2019-10-16 16:00:41 +0300

    [SQUASH] BoostFramework: To Enhance performance.
    DennySPb: thx CAF for pulling owl on the globe
    
    this is squash of the following commits:
    
    BoostFramework: To Enhance performance.
    framework/base: add parallel verifyV1
    Add perf hint for rotation latency
    Add perf hint for screen rotation animation
    IOP/UXE: This change is to enable the IOP and UXE
    BoostFramework: Move property checks to pass sepolicy
    IOP : Adding support for UXPerf Class
    BoostFramework: Clean up code
    Performance: Activity Trigger frameworks support
    Removed activity trigger whitelist related support
    Disable swipe boost and preferred apps for certain cases
    Revert "IOP/UXE: This change is to enable the IOP and UXE"
    Revert "IOP : Adding support for UXPerf Class"
    Add Game boost & modify launchboostv2 invocation.
    Add perfGetFeedback api support from framework
    Modify game detection logic
    IOP/UXE: This change is related to IOP and UXE Feature.
    Support the perflock request from system/priv apps whose domain are untrusted
    Disable swipe boost and preferred apps for certain cases
    framework: Adding support for perf get prop in Boostframework
    BoostFramework : Modified Start Proc trigger and added Kill Hint.
    Boostframework: Adding reserved hint types for launch boost
    Moving property to iop hal
    Add perf hint for performance mode
    Add start support for Activity Trigger
    Perf: Change for AGPE
    perf: Convert Horizontal Scroll to GestureFlingBoost.
    ...and some other up to LA.UM.8.3.r1-06100-sdm845.0 CAF tag
    
    Change-Id: I8811ce5fa3428358bcb540d7d48fa6601e064e70
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- services/core/java/com/android/server/wm/ActivityStarter.java
+++ services/core/java/com/android/server/wm/ActivityStarter.java
@@ -115,6 +115,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.service.voice.IVoiceInteractionSession;
 import android.text.TextUtils;
+import android.util.BoostFramework;
 import android.util.ArraySet;
 import android.util.EventLog;
 import android.util.Pools.SynchronizedPool;
@@ -200,6 +201,8 @@ class ActivityStarter {
 
     private IVoiceInteractionSession mVoiceSession;
     private IVoiceInteractor mVoiceInteractor;
+
+    public BoostFramework mPerf = null;
 
     // Last activity record we attempted to start
     private final ActivityRecord[] mLastStartActivityRecord = new ActivityRecord[1];
@@ -448,6 +451,7 @@ class ActivityStarter {
         mSupervisor = supervisor;
         mInterceptor = interceptor;
         reset(true);
+        mPerf = new BoostFramework();
     }
 
     /**
@@ -1703,6 +1707,12 @@ class ActivityStarter {
         if (mStartActivity.resultTo == null && mInTask == null && !mAddingToTask
                 && (mLaunchFlags & FLAG_ACTIVITY_NEW_TASK) != 0) {
             newTask = true;
+            String packageName= mService.mContext.getPackageName();
+            if (mPerf != null) {
+                mStartActivity.perfActivityBoostHandler =
+                    mPerf.perfHint(BoostFramework.VENDOR_HINT_FIRST_LAUNCH_BOOST,
+                                        packageName, -1, BoostFramework.Launch.BOOST_V1);
+            }
             result = setTaskFromReuseOrCreateNewTask(taskToAffiliate);
         } else if (mSourceRecord != null) {
             result = setTaskFromSourceRecord();
@@ -2391,6 +2401,12 @@ class ActivityStarter {
                 mSourceRecord.getTaskRecord())) {
             Slog.e(TAG, "Attempted Lock Task Mode violation mStartActivity=" + mStartActivity);
             return START_RETURN_LOCK_TASK_MODE_VIOLATION;
+        }
+        String packageName= mService.mContext.getPackageName();
+        if (mPerf != null) {
+            mStartActivity.perfActivityBoostHandler =
+                mPerf.perfHint(BoostFramework.VENDOR_HINT_FIRST_LAUNCH_BOOST,
+                                    packageName, -1, BoostFramework.Launch.BOOST_V1);
         }
 
         final TaskRecord sourceTask = mSourceRecord.getTaskRecord();
