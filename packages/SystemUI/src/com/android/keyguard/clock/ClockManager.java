5135daf82d7177b0735a04e818056045a3d482b1
SirRGB <sirrgb@proton.me>
2023-05-21 15:44:50 +0200

    Revert "Add Custom Spectrum Analog Clock"
    This reverts commit 9e7ba2baa24acd10e264e3a7545d3e9a61e55579.

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -163,7 +163,6 @@ public final class ClockManager {
         addBuiltinClock(() -> new LineageClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new FluidClockController(res, layoutInflater, colorExtractor));
-        addBuiltinClock(() -> new SpectrumClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TuxClockController(res, layoutInflater, colorExtractor));
 
